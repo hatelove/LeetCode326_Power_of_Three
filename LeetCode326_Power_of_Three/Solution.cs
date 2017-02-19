636aeda87dcaf9619a5ad987a788cf25ef0e8afe
hatelove <jay.hatelove@gmail.com>
2017-02-19 16:34:45 +0800

    Initialized

---
+++ LeetCode326_Power_of_Three/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode326_Power_of_Three
+{
+    public class Solution
+    {
+        public bool IsPowerOfThree(int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LeetCode326_Power_of_Three/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode326_Power_of_Three
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
